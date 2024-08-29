1167da312aad016e68ffb4554360717ef6b6bbab
Stanley Shih <stanley155292@gmail.com>
2024-08-29 15:58:46 +0800

    Fix Port issue

--- app.py
+++ app.py
@@ -90,6 +90,6 @@ def run_bot():
         print(f"Bot 啟動失敗: {e}")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8081))
     threading.Thread(target=lambda: app.run(host='0.0.0.0', port=port)).start()
     run_bot()
